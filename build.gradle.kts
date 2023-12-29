9e880de8c1a63832937836c34a24cbf4e0063052
Evgenii <jenyhlevnoy@gmail.com>
2023-12-30 02:20:42 +0400

    Changed gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.kotlin.jvm") version "1.9.20"
     id("org.jetbrains.intellij") version "1.16.1"
 }
 
@@ -48,15 +48,6 @@ intellij {
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
     patchPluginXml {
         sinceBuild.set("233")
         untilBuild.set("241.*")
